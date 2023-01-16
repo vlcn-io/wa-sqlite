fa5b57c6216389c81078a6bf64c86acf63dd3f19
Matt <1009003+tantaman@users.noreply.github.com>
2023-01-16 16:25:34 -0500

    webext init

--- src/core_init.c
+++ src/core_init.c
@@ -1,8 +1,8 @@
-int sqlite3_nostdextension_init(sqlite3 *db, char **pzErrMsg,
-                                const sqlite3_api_routines *pApi);
+int sqlite3_webext_init(sqlite3 *db, char **pzErrMsg,
+                        const sqlite3_api_routines *pApi);
 
 int core_init(const char *dummy)
 {
-  return sqlite3_auto_extension((void *)sqlite3_nostdextension_init);
+  return sqlite3_auto_extension((void *)sqlite3_webext_init);
   // return SQLITE_OK;
 }
